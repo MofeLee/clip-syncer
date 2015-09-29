c228b4a74e9be38afd3802a21c9728f62f17edd8
Mofe Lee <mofe@me.com>
2015-09-29 17:38:40 +0800

    add registerShortcut function

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 'use strict';
 const app = require('app');
 const BrowserWindow = require('browser-window');
+const globalShortcut = require('global-shortcut');
+const clipboard = require('clipboard');
 
 // report crashes to the Electron project
 require('crash-reporter').start();
@@ -11,36 +13,52 @@ require('electron-debug')();
 // prevent window being garbage collected
 let mainWindow;
 
-function onClosed() {
-	// dereference the window
-	// for multiple windows store them in an array
-	mainWindow = null;
-}
-
-function createMainWindow() {
-	const win = new BrowserWindow({
-		width: 600,
-		height: 400
-	});
-
-	win.loadUrl(`file://${__dirname}/index.html`);
-	win.on('closed', onClosed);
-
-	return win;
-}
-
 app.on('window-all-closed', () => {
-	if (process.platform !== 'darwin') {
-		app.quit();
-	}
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
 });
 
 app.on('activate-with-no-open-windows', () => {
-	if (!mainWindow) {
-		mainWindow = createMainWindow();
-	}
+  if (!mainWindow) {
+    mainWindow = createMainWindow();
+  }
 });
 
 app.on('ready', () => {
-	mainWindow = createMainWindow();
+  mainWindow = createMainWindow();
+  registerShortcut();
 });
+
+function onClosed() {
+  // dereference the window
+  // for multiple windows store them in an array
+  mainWindow = null;
+}
+
+function createMainWindow() {
+  const win = new BrowserWindow({
+    width: 600,
+    height: 400
+  });
+
+  win.loadUrl(`file://${__dirname}/index.html`);
+  win.on('closed', onClosed);
+
+  return win;
+}
+
+function registerShortcut(){
+  let ret = globalShortcut.register('alt+s', function() {
+    console.log(clipboard.readText());
+  });
+
+  if (!ret) {
+    console.log('registration failed');
+    return;
+  }
+
+  // Check whether a shortcut is registered.
+  console.log(globalShortcut.isRegistered('alt+s'));
+  return ret;
+}
